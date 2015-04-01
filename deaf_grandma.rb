dadb08b069001b4af4a0c4ce88bf9d731d1dafbe
Dmitry Sky <dmitry.sky@icloud.com>
2015-04-01 17:41:11 -0600

    touch-up

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -2,28 +2,31 @@ ready_to_quit = false
 
 puts "HEY KID!"
 
-count = 0
+goodbye = 0
   until ready_to_quit
-    if count < 0
-      count = 0
-    end
 
     input = gets.chomp
+    
     if input == ""
       puts "WHAT?!"
-      count -= 1
     elsif input == input.downcase
       puts "SPEAK UP, KID!"
-      count -= 1
     elsif input == "GOODBYE!"
-      puts "LEAVING SO SOON?"
-      count += 1
+      goodbye += 1
+      
+      if goodbye == 2
+        ready_to_quit = true
+      else 
+        puts "LEAVING SO SOON?"
+      end
+    
     elsif input == input.upcase
-      puts "NO, NOT SINCE 1946!"
-      count -= 1 
+      puts "NO, NOT SINCE 1946!" 
     end
-    ready_to_quit = true if count == 2
+
   end
 
 puts "LATER, SKATER!"
+
+
 
