0e10cf7a00b7b451b0ea08d2895d0e07f8e32dc4
Dmitry Sky <dmitry.sky@icloud.com>
2015-04-02 15:05:49 -0600

    added grandma class and cleaned up

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -1,36 +1,3 @@
-# ready_to_quit = false
-
-# puts "HEY KID!"
-
-# goodbye = 0
-#   until ready_to_quit
-
-#     input = gets.chomp
-    
-#     if input == ""
-#       puts "WHAT?!"
-#     elsif input == input.downcase
-#       puts "SPEAK UP, KID!"
-#     elsif input == "GOODBYE!"
-#       goodbye += 1
-      
-#       if goodbye == 2
-#         ready_to_quit = true
-#       else 
-#         puts "LEAVING SO SOON?"
-#       end
-    
-#     elsif input == input.upcase || input.capitalize
-#       puts "NO, NOT SINCE 1946!" 
-#     end
-
-#   end
-
-# puts "LATER, SKATER!"
-
-
-
-
 puts "HEY KID!"
 
 goodbye = 0
